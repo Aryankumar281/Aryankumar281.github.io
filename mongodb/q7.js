36728777723f8a775295090ffcf81cc61725bf2d
AryanKumar281 <kumararyan5214@gmail.com>
2025-07-01 16:23:39 +0530

    updated

--- mongodb/q7.js
+++ mongodb/q7.js
@@ -63,8 +63,58 @@ db.studentinfo.aggregate([
       as: "marks",
     },
   },
-  {$unwind:"$marks"},
-  
-  {$group:{_id:"$marks.term",
-    AvgScore:{$avg:"$marks.score"}}}
+  { $unwind: "$marks" },
+
+  { $group: { _id: "$marks.term", AvgScore: { $avg: "$marks.score" } } },
+]);
+
+
+
+
+db.employees.aggregate([
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      salary: 1,
+      Grade: { $cond: [{ $gt: ["$salary", 3000] }, "Grade A", "Grade B"] },
+    },
+  },
+]);
+
+// out stores the result in other collection
+db.employees.aggregate([
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      salary: 1,
+      Grade: { $cond: {
+            if:{$gt:["$salary",3000]},
+            then:"Grade A",
+            else:"Grade B"
+        },
+    },
+  },
+},
+{$out:"GradeWiseSalary"}
+]);
+
+
+
+db.createView("salaryView","employees",[
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      salary: 1,
+      department:1,
+      Grade: { $cond: {
+            if:{$gt:["$salary",3000]},
+            then:"Grade A",
+            else:"Grade B"
+        },
+    },
+  },
+}
 ]);
